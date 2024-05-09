e0df7396bdd4d94823925d770a00d63d583c7853
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-05-09 15:55:04 +0530

    STYLE: landing page, categories and cart styling done

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import { Button } from "./ui/button";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
+}
+
+const NavItem = ({ isOpen, isAnyOpen, category, handleOpen }: NavItemProps) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {category.label}
+          <ChevronDown
+            className={cn("h-4 w-4 transition-all text-muted-foreground", {
+              "-rotate-180": isOpen,
+            })}
+          />
+        </Button>
+      </div>
+
+      {isOpen ? (
+        <div
+          className={cn("absolute top-full text-sm text-muted-foreground", {
+            "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen,
+          })}
+        >
+          <div
+            className="absolute indent-0 top-1/2 bg-white shadow"
+            aria-hidden="true"
+          />
+
+          <div className="relative bg-white rounded-lg shadow shadow-gray-300">
+            <div className="mx-auto max-w-4xl px-8">
+              <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-8">
+                <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-4 gap-y-4">
+                  {category.featured.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative text-base sm:text-sm"
+                    >
+                      <div className="relative rounded-lg bg-green-100 hover:shadow hover:shadow-green-500 flex justify-start py-4 px-3">
+                        <Image
+                          src={item.imageUrl}
+                          alt="product category image"
+                          width={64}
+                          height={58}
+                          className=""
+                        />
+                        <div className="flex flex-col justify-center pl-2">
+                          <Link
+                            href={item.href}
+                            className="block font-medium text-gray-900"
+                          >
+                            {item.name}
+                          </Link>
+                          <p className="mt-1" aria-hidden="true">
+                            Shop Now
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default NavItem;
---
+++ src/config/index.ts
@@ -0,0 +1,67 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "Healthcare",
+    value: "health_care" as const,
+    featured: [
+      {
+        name: "Elderly Care",
+        href: "#",
+        value: "elderly_care" as const,
+        imageUrl: "/categories/elderly-care.webp"
+      },
+      {
+        name: "Personal Care",
+        href: "#",
+        value: "personal_care" as const,
+        imageUrl: "/categories/personal-care.webp"
+      },
+      {
+        name: "Skin Care",
+        href: "#",
+        value: "skin_care" as const,
+        imageUrl: "/categories/skin-care.webp"
+      },
+      {
+        name: "Mother and Baby Care",
+        href: "#",
+        value: "mother_and_baby_care" as const,
+        imageUrl: "/categories/mother-and-baby-care.webp"
+      },
+      {
+        name: "Accesories and Wearables",
+        href: "#",
+        value: "accesories_and_wearables" as const,
+        imageUrl: "/categories/accesories-and-wearables.webp"
+      },
+      {
+        name: "Fitness Supplements",
+        href: "#",
+        value: "fitness_supplements" as const,
+        imageUrl: "/categories/fitness-supplements.webp"
+      },
+      {
+        name: "Sexual Wellness",
+        href: "#",
+        value: "sexual_wellness" as const,
+        imageUrl: "/categories/sexual-wellness.webp"
+      },
+      {
+        name: "Diabetic Care",
+        href: "#",
+        value: "diabetic_care" as const,
+        imageUrl: "/categories/diabetic-care.webp"
+      },
+      {
+        name: "Home Care",
+        href: "#",
+        value: "home_care" as const,
+        imageUrl: "/categories/home-care.webp"
+      },
+    ],
+  },
+  {
+    label: "Health Food and Drinks",
+    value: "health_food_and_drinks" as const,
+    featured: [],
+  },
+];
